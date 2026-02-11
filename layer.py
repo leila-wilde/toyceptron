94a9bed06cadb7bb25e33449c3b61374245762e3
leila-wilde <leila.wilde@laplateforme.io>
2026-02-11 11:23:26 +0000

    refactor: add comments to classes layer and network to explain their function

--- layer.py
+++ layer.py
@@ -1,5 +1,8 @@
 # Layer.py
 
+# store many neurons
+# has forward(input_vector) method that runs all neurons and returns 
+
 class Layer:
     def __init__(self):
         pass
--- network.py
+++ network.py
@@ -1,5 +1,8 @@
 # Network.py
 
+# store many layers
+# has feedforward(input_vector) method that passes data through all
+
 class Network:
     def __init__(self):
         pass
