317d44ec623891a99f84b745b9b5d5d5c8d6ea75
leila-wilde <leila.wilde@laplateforme.io>
2026-02-11 11:31:14 +0000

    feat: define neuron class that takes a list of weights and a bias in the constructor, and implement forward method to calculate the neuron output. Calculate output by multiplying each input with each weight and then adding the bias.

--- neuron.py
+++ neuron.py
@@ -1,5 +1,22 @@
 # Neuron.py
 
+# neuron class to store weights and bias
+# has forward(input_vector) method that calculates output
+
 class Neuron:
-    def __init__(self):
-        pass
+    def __init__(self, weights : list, bias=0.0):
+        self.weight = weights
+        self.bias = bias
+
+    def forward(self, inputs):
+        """
+        calculate neuron output: (input[0]*weight[0]) + (input[1]*weight[1]) + ... + bias  
+        args inputs takes a list [x1, x2, x3, ...]
+        returns the raw output before activation
+        """
+        for i in range(len(inputs)):
+            weight_sum += inputs[i] * self.weights[i]
+
+        output = weight_sum + self.bias
+
+        return output
