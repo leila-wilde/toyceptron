e5df9aa97e120cef9a3d89dade78a7e867a3667a
leila-wilde <leila.wilde@laplateforme.io>
2026-02-15 23:14:39 +0000

    feat: add basic activation fuctions

--- activation.py
+++ activation.py
@@ -1,3 +1,15 @@
 # activation.py - activation functions
-# implement identity, threshold, ReLU (sigmoid is given)
+# implement identity, threshold, ReLU (sigmoid is given in main)
 # method names - act_relu, act_threshold, act_identity
+
+def act_relu(x):
+    # f(x) = max(0,x)
+    return max(0, x)
+
+def act_threshold(x):
+    # f(x) = 1 if x > 0 else 0
+    return 1 if x >= 0 else 0
+
+def act_identity(x):
+    # f(x) = x
+    return x
