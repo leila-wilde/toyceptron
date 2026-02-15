31fcfb5cd6deb2490c7ca4a7cf16a3dd272a4939
leila-wilde <leila.wilde@laplateforme.io>
2026-02-15 23:13:27 +0000

    feat: add layer.py class to create and store many neurons. Forward method passes input through all neurons in the layer

--- layer.py
+++ layer.py
@@ -1,8 +1,20 @@
 # Layer.py
+# an ordered collection of identical neurons that :
+# - creates/stores many neurons
+# - applies a vector of input to all its neurons and produce a vector of output
 
-# store many neurons
-# has forward(input_vector) method that runs all neurons and returns 
+from neuron import Neuron
 
 class Layer:
-    def __init__(self):
-        pass
+    def __init__(self, weights_list : list, biases_list : list ):
+        self.neurons = [] # init empty list
+        for i in range(len(weights_list)):
+            neuron = Neuron(weights=weights_list[i], bias=biases_list[i])
+            self.neurons.append(neuron)
+
+    def forward(self, inputs : list):
+        outputs = []
+        for neuron in self.neurons:
+            output = neuron.forward(inputs)
+            outputs.append(output)
+        return outputs
