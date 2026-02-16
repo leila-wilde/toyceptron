117e03aea50be820d5b2e3b6220129be7ee5efe8
leila-wilde <leila.wilde@laplateforme.io>
2026-02-16 00:08:02 +0000

    feat: add feedforward method to network class that acts like a pipeline that feeds each layers output after activation as input to the next layer

--- network.py
+++ network.py
@@ -21,3 +21,21 @@ class Network:
         # create new layer with weights and biases
         layer =  Layer(weights_list=weights, biases_list=biases)
         self.layers.append(layer)
+    
+    def feedforward(self, inputs): 
+        # start will original input
+        outputs = inputs
+        
+        # pass through each layer one by one
+        for layer in self.layers:
+            # get raw outputs from layer
+            raw_outputs = layer.forward(outputs)
+            
+            #apply activation fuction to each output
+            outputs = [self.activation(i) for i in raw_outputs]
+        
+        return outputs
+        # Network is like a pipeline:
+        # input -> layer 1 -> activate -> layer 2 -> activate -> layer 3 -> activate -> output
+        # each layer's output (after activation) becomes the next layer's input
+
