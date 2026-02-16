55056234d9ba80ddd4846576337fb07510177b29
leila-wilde <leila.wilde@laplateforme.io>
2026-02-16 00:04:33 +0000

    feat: add Network class that creates a network with a given input size and activation function. add() method to add a new layer to the network and store as list self.layers

--- network.py
+++ network.py
@@ -3,6 +3,21 @@
 # store many layers
 # has feedforward(input_vector) method that passes data through all
 
+from layer import Layer
+
 class Network:
-    def __init__(self):
-        pass
+    def __init__(self, input_size, activation):
+        self.input_size = input_size 
+        self.activation = activation 
+        self.layers = [] # empty list to hold layers
+
+    def add(self, weights, biases): # add layers
+        """
+        add a new layer to the network
+        args: 
+        - list of weight lists for each neuron
+        - list of biases for each neuron 
+        """
+        # create new layer with weights and biases
+        layer =  Layer(weights_list=weights, biases_list=biases)
+        self.layers.append(layer)
